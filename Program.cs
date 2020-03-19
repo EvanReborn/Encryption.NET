b011c1adab55f45aed5348ec38ba045bf0a16282
Evan <56363914+Retransition@users.noreply.github.com>
2020-03-19 16:23:57 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Vigilante;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // End-To-End encryption example
+            Encryption.StartSession();
+
+            SecureWebClient.SendEncrypteValues("https://yourdomain.ltd", new NameValueCollection 
+            {
+                ["example"]     = Encryption.Encrypt("data"),
+                ["example2"]    = Encryption.Encrypt("data")
+            }, "CUSTOM_USERAGENT");
+
+            Encryption.EndSession();
+
+            // Grab server-sided variables
+            string variable = Variables.GrabVariable("https://yourdomain.ltd", "ExampleName", "YourPassword", "CUSTOM_USERAGENT");
+
+            // Some of the improved WebClient methods
+            SecureWebClient.DownloadString("https://thirdpartydomain.ltd"/*, "useragent"*/);
+            SecureWebClient.DownloadData("https://thirdpartydomain.ltd"/*, "useragent"*/);
+            SecureWebClient.UploadValues("https://thirdpartydomain.ltd", new NameValueCollection { ["example"] = "data" }/*, "useragent"*/);
+        }
+    }
+}
